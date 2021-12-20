f5ef432d0aa36517458ab47de316f086f4fe9e09
robertswh <wilhroberts@gmail.com>
2021-12-20 17:41:21 +0000

    working email sender

--- send_mail.py
+++ send_mail.py
@@ -1,18 +1,27 @@
 import smtplib
 
-sender = 'example@gmail.com'
-receiver = 'example@gmail.com'
+gmail_user = 'secretsandra00@gmail.com'
+gmail_password = 'cyfrinachsandra'
 
-message = f"""From: From Person <{sender}>
-To: To Person <{receiver}>
-Subject: SMTP e-mail test
+sent_from = gmail_user
+to = ['wilhroberts@gmail.com']#, 'person_b@gmail.com']
+subject = 'testing'
+body = 'this is a test'
 
-This is a test e-mail message.
-"""
+email_text = """\
+From: %s
+To: %s
+Subject: %s
+
+%s
+""" % (sent_from, ", ".join(to), subject, body)
 
 try:
-    smtpObj = smtplib.SMTP('localhost')
-    smtpObj.sendmail(sender, receiver, message)         
-    print("Successfully sent email")
-except SMTPException:
-    print("Error: unable to send email")
+    smtp_server = smtplib.SMTP_SSL('smtp.gmail.com', 465)
+    smtp_server.ehlo()
+    smtp_server.login(gmail_user, gmail_password)
+    smtp_server.sendmail(sent_from, to, email_text)
+    smtp_server.close()
+    print ("Email sent successfully!")
+except Exception as ex:
+    print ("Something went wrong….",ex)
